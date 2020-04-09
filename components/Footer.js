8073ba279609c3a9311eed1cf44555e761abd3e0
Victor Guadarrama <victor.guadarrama3@gmail.com>
2020-04-09 14:45:19 -0500

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <a href="https://zeit.co" target="_blank" rel="noopener noreferrer">
+          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+        </a>
+      </footer>
+
+      <style jsx>
+        {`
+          footer {
+            width: 100%;
+            height: 100px;
+            border-top: 1px solid #eaeaea;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          footer img {
+            margin-left: 0.5rem;
+          }
+
+          footer a {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          a {
+            color: inherit;
+            text-decoration: none;
+          }
+        `}
+      </style>
+    </>
+  );
+}
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,45 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Footer from "../components/Footer";
 
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>VictorGu</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Welcome to <a href="#">victorgu.com!</a>
       </h1>
 
       <p className="description">
-        Get started by editing <code>pages/index.js</code>
+        Software Engineer currently at <code>/home</code>
       </p>
 
       <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
+        <a href="#" className="card">
+          <h3>Software Dev &rarr;</h3>
+          <p>Acá va info sobre tecnologías de desarrollo de software.</p>
         </a>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
+        <a href="#" className="card">
+          <h3>Links</h3>
+          <p>Sitios interesantes blah blah blah blah blah blah blah </p>
         </a>
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
+        <a href="#" className="card">
+          <h3>Básquetbol &rarr;</h3>
+          <p>Basket blah blah blah blah blah blah blah blah blah blah </p>
         </a>
 
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
+        <a href="#" className="card">
+          <h3>Literatura &rarr;</h3>
+          <p>blah blah blah blah blah blah blah blah blah blah blah </p>
         </a>
       </div>
     </main>
-
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
+    <Footer />
 
     <style jsx>{`
       .container {
@@ -74,30 +58,6 @@ const Home = () => (
         flex-direction: column;
         justify-content: center;
         align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
       }
 
       .title a {
@@ -198,6 +158,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
